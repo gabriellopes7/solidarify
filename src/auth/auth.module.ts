63e742d4f7d69d9e6e2b1fcb792b5b670c3b623b
Gabriel Ladeira <gabriellopes7@yahoo.com>
2023-09-22 13:29:02 -0300

    accessToken implementation

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,25 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async signIn(signInDto: ISignInInterface): Promise<any> {
     const user = await this.usersService.findByEmail(signInDto.email);
     if (user?.password !== signInDto.password) {
       throw new UnauthorizedException();
     }
-    const { ...result } = user;
-    return result;
+    const payload = { sub: user.id, email: user.email, type: user.userType };
+    return {
+      access_token: await this.jwtService.signAsync(payload, {
+        secret: process.env.JWT_SECRET,
+        expiresIn: process.env.JWT_EXPIRES,
+      }),
+    };
   }
 }
